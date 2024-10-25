b5726a2ade814aee29a55d8e5fcff899fc7bd68f
Mihai Mureșan <mihaimuresan592003@gmail.com>
2024-10-25 22:50:12 +0300

    feat: fetch schedule for any line and handle not found case

--- index.js
+++ index.js
@@ -1,62 +1,105 @@
 import puppeteer from "puppeteer";
 
-const browser = await puppeteer.launch({ headless: false });
-const page = await browser.newPage();
-
-await page.goto(
-  "https://ctpcj.ro/index.php/ro/orare-linii/linii-urbane/linia-35",
-  { waitUntil: "networkidle2" }
-);
-
-await page.setViewport({ width: 1024, height: 1024 });
-
-await page.waitForSelector("table.tztable");
-
-let schedule = await page.evaluate(() => {
-  const tables = document.querySelectorAll("table.tztable");
-  let currentTable;
-
-  const today = new Date().getDay();
-  switch (today) {
-    case 7:
-      currentTable = tables[2];
+async function getLineUrl(lineIdx) {
+  let url = "https://ctpcj.ro/index.php/ro/orare-linii/";
+  switch (true) {
+    case lineIdx[0] === "M":
+      url += "linii-metropolitane/";
       break;
-    case 6:
-      currentTable = tables[1];
+    case lineIdx.at(-1) === "N":
+      url += "transport-noapte/";
+      break;
+    case lineIdx.at(-1) === "E":
+      url += "linie-expres/";
+      break;
+    case lineIdx.slice(0, 2) === "99":
+      url += "linii-supermarket/";
       break;
     default:
-      currentTable = tables[0];
+      url += "linii-urbane/";
+      break;
   }
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-  const rows = Array.from(
-    currentTable.querySelector("tbody").querySelectorAll("tr")
-  );
-  const stations = Array.from(currentTable.querySelectorAll("th"));
+  await page.goto(url, { waitUntil: "networkidle2" });
+  await page.waitForSelector("div.tzPortfolio");
 
-  let result = [
-    { station: stations[0].textContent.trim(), departures: [] },
-    { station: stations[1].textContent.trim(), departures: [] },
-  ];
+  let lineUrl = await page.evaluate((lineIdx) => {
+    const anchors = document.querySelectorAll("div.tzPortfolio a");
+    for (let anchor of anchors) {
+      if (anchor.textContent.includes(" " + lineIdx)) {
+        return { result: anchor.href, error: null };
+      }
+    }
+    return { result: null, error: "404 Not Found" };
+  }, lineIdx);
 
-  rows.map((row) => {
-    const cells = Array.from(row.querySelectorAll("td"));
-    let departures = cells.map((cell) => {
-      const timeString = cell.textContent.trim();
+  await browser.close();
+  return lineUrl;
+}
 
-      let hours = Number(timeString.slice(0, 2));
-      let minutes = Number(timeString.slice(3, 5));
+async function getLineSchedule(lineIdx) {
+  let { result: url, error } = await getLineUrl(lineIdx);
+  if (error) {
+    return { result: null, error: error };
+  }
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-      let time = new Date();
-      time.setHours(hours, minutes, 0, 0);
+  await page.goto(url, { waitUntil: "networkidle2" });
+  await page.waitForSelector("table.tztable");
 
-      return time;
+  let schedule = await page.evaluate(() => {
+    const tables = document.querySelectorAll("table.tztable");
+    let currentTable;
+
+    const today = new Date().getDay();
+    switch (today) {
+      case 7:
+        currentTable = tables[2];
+        break;
+      case 6:
+        currentTable = tables[1];
+        break;
+      default:
+        currentTable = tables[0];
+    }
+
+    const rows = Array.from(
+      currentTable.querySelector("tbody").querySelectorAll("tr")
+    );
+    const stations = Array.from(currentTable.querySelectorAll("th"));
+
+    let result = stations.map((station) => ({
+      station: station.textContent.trim(),
+      departures: [],
+    }));
+
+    rows.forEach((row) => {
+      const cells = Array.from(row.querySelectorAll("td"));
+      cells.forEach((cell, index) => {
+        const timeString = cell.textContent.trim().slice(0, 5);
+
+        let [hours, minutes] = timeString.split(":").map(Number);
+
+        let time = new Date();
+        time.setHours(hours, minutes);
+
+        result[index].departures.push(time.toTimeString());
+      });
     });
-    result[0].departures.push(departures[0]);
-    result[1].departures.push(departures[1]);
+    return { result: result, error: null };
   });
-  return result;
-});
 
-console.log(schedule);
+  await page.close();
+  await browser.close();
+  return schedule;
+}
 
-await browser.close();
+const { result: schedule, error } = await getLineSchedule("35");
+if (error) {
+  console.error(`Error: ${error}`);
+} else {
+  console.log(schedule);
+}
